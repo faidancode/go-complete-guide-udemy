b49ca8ac13bffc53e5a87dc22f0fd3cfa68f7d05
faidancode <faidancode393@gmail.com>
2026-02-09 13:50:59 +0700

    7-receiver-functions

--- deck.go
+++ deck.go
@@ -4,7 +4,9 @@ import "fmt"
 
 type deck []string
 
-// () redeiver
+// () receiver
+// any var of type deck gat access to the print method
+// d is an actual copy, every var can call this function
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
