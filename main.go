ada45feed8e6899263c1f2f429eb0c69580d2e2c
faidancode <faidancode393@gmail.com>
2026-02-09 13:34:49 +0700

    4-slices-and-for-loops

--- main.go
+++ main.go
@@ -3,8 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	card := newCard()
-	fmt.Println(card)
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Four of Clubs")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
 }
 
 func newCard() string {
